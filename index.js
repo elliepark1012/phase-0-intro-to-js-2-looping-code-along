d4166efe7a4cded54d8cf9b2b4b7ab70d5bc8ed3
Ellie <elliepark1012@gmail.com>
2022-07-06 23:22:48 -0400

    commit tree 82baed1ec1bd25ad1e46e3b910fe39907286647d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    let message = [];
+    for(let i=0; i < name.length; i++){
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)    
+    }
+
+    return message
+    
+}
+
+function countDown(number) {
+    
+    while (number > 0){
+        console.log(number);
+        number--;
+    }
+    console.log(number)
+}
+
+
+
